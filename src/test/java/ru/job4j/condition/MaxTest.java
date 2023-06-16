8028e14cdadd6e33dc9aef354f98fb1b96b6dee5
musfan1 <musfan1@mail.ru>
2023-06-16 13:48:38 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,88 @@ public class MaxTest {
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenFirst1Second2Third3max3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirst100Second20Third3max100() {
+        int first = 100;
+        int second = 20;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirst1Second20Third3max20() {
+        int first = 1;
+        int second = 20;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 20;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirst10Second10Third10max10() {
+        int first = 10;
+        int second = 10;
+        int third = 10;
+        int result = Max.max(first, second, third);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirst1Second2ThirdFourth4max4() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirst100Second20Third3Fourth4max100() {
+        int first = 100;
+        int second = 20;
+        int third = 3;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirst1Second20Third3Fourth4max20() {
+        int first = 1;
+        int second = 20;
+        int third = 3;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 20;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirst10Second10Third10Fourth10max10() {
+        int first = 10;
+        int second = 10;
+        int third = 10;
+        int fourth = 10;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
 }
